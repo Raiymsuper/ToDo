fcb0c1a9e74694450ab3c2267cd154a4377b1f45
Raiymbek K <raiymbek200516@gmail.com>
2025-02-16 05:30:49 +0500

    Fixed database problems 6 try

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"database/sql"
 	"embed"
 	"log"
-	"os"
-	"path/filepath"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -19,38 +17,11 @@ var assets embed.FS
 var db *sql.DB
 
 func InitDB() {
-	execPath, err := os.Executable()
-	if err != nil {
-		log.Fatal("Failed to get executable path:", err)
-	}
-
-	execDir := filepath.Dir(execPath)
-
-	dbPath := filepath.Join(execDir, "tasks.db")
-
-	if _, err := os.Stat(dbPath); os.IsNotExist(err) {
-		log.Println("Database not found, copying from project root...")
-
-		projectDbPath := "tasks.db"
-		err = copyFile(projectDbPath, dbPath)
-		if err != nil {
-			log.Fatal("Failed to copy database file:", err)
-		}
-	}
-
-	// Открываем БД
-	db, err = sql.Open("sqlite", dbPath+"?_foreign_keys=on")
+	var err error
+	db, err = sql.Open("sqlite", "tasks.db")
 	if err != nil {
 		log.Fatal("Failed to open database:", err)
 	}
-}
-
-func copyFile(src, dst string) error {
-	input, err := os.ReadFile(src)
-	if err != nil {
-		return err
-	}
-	return os.WriteFile(dst, input, 0644)
 }
 
 func main() {
